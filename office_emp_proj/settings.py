11ebc4b01ef925664e25d0bbf551a0a146518756
Avishkar <avishkar2108@gmail.com>
2022-12-20 16:27:48 +0530

    vercel setting added

--- office_emp_proj/settings.py
+++ office_emp_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3ezlqp6sv!(f+f*avuv^193lj60&xkpo29rt0&n64jzw@whqny
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATICFILES_DIRS  = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
